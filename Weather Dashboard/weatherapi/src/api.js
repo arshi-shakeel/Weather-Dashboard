25c3a50d8e48d2f02c65cf55098f5fb34edc52df
arshi-shakeel <126773149+arshi-shakeel@users.noreply.github.com>
2024-03-01 21:10:25 +0530

    Add files via upload

---
+++ Weather Dashboard/weatherapi/src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd92a7f2fb8mshcea15f0a2c45fe0p19a20ajsn634902b40fb1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "69ae21818bed355b4423baba5770fa0d"
